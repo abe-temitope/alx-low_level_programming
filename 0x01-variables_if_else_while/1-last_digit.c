dd52ed64c191aebd6a686106740770f2be6ea6e8
abetemitope <bestm4305@gmail.com>
2023-08-12 23:05:46 -0700

    The opening curly braces must be on the next line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,7 @@
  *
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
@@ -16,13 +17,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
 
-	if (ld > 5) {
+	if (ld > 5)
+       	{		
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-	} else if (ld == 0) {			
-		printf("Last digit of %d is %d and is 0\n", n, ld);
-	} else if (ld < 6 && ld != 0) {			
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-	}
-			
+	} else if (ld == 0){
+		printf("Last digit of %d is %d and is 0\n", n, ld)
+	} else if (ld < 6 && ld != 0)
+	{				
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);				
+	}																
 	return (0);
 }
