73052f8bc1ab53a0b6c0d5d94d2ec9baeef10881
abetemitope <bestm4305@gmail.com>
2023-08-13 02:00:24 -0700

    Print number in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* *
+ * main - Write score
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");	
+	return (0);
+}
