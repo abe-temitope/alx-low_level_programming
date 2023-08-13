5ba53dcd41fcd7b7a5bd2ce4c43c3999d8e68cdb
abetemitope <bestm4305@gmail.com>
2023-08-13 05:36:36 -0700

    Print the base the base 10.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * mian - A single digit of base ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(\n);
+	return (0);
+}	
