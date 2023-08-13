268bb8d475aff7bc5b04c350ffb610906cb137b6
abetemitope <bestm4305@gmail.com>
2023-08-13 00:02:17 -0700

    Print alphabet to standard output.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 /**
- *  * main - Pritn alphabet
- *   *
- *    * Return: 0
- *     */
+ * main - Pritn alphabet
+ *
+ * Return: 0
+ */
 int main(void)
 {
-		char z = 'a';
+	char z = 'a';
 
-			while (z <= 'z')
-					{
-								putchar(z);
-										z++;
-											}
-				putchar('\n');
-					return (0);
+	while (z <= 'z')
+	{
+		putchar(z);
+		z++;
+	}
+	putchar('\n');
+	return (0);
 }
