e287f49df5f63c3eb29ee4a5b7c1ee2e0fa7d8f2
abetemitope <bestm4305@gmail.com>
2023-08-13 10:44:42 -0700

    Print single digit number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
