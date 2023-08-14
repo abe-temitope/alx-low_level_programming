2f76e107a13dd81698e094d919416ce32eaa8869
abetemitope <bestm4305@gmail.com>
2023-08-14 00:44:36 -0700

    Made some little corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,19 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *     *
- *      * Return: Always 0 (Success)
- *       */
-
+ * main - Run all functions
+ *
+ * Description: Change standard error message.
+ * Return: Return 1 and exit
+ */
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	int l;
+
+	l =
+	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+	return (1);
 }
