17df393bad9992714ba29f7a0087004ed7125f82
abetemitope <bestm4305@gmail.com>
2023-08-16 05:13:04 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int letter = 'a';
-	int num = 0;
+	int letter, num;
 
+	int num = 0;
 	while (num < 10)
 	{
+		letter = 'a';
 		while (letter <= 'z')
 		{
 			_putchar(letter);
