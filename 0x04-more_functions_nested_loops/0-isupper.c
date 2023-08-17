6a57e94baad6c3bb24abe120fe9f77eba4c30822
abetemitope <bestm4305@gmail.com>
2023-08-16 22:58:05 -0700

    More functions and nested loop.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - Find if an <F11>argument letter is uppercase
+ * @c: the given letter
+ *
+ * Return: Return 1 if c is uppercase, 0 if lowercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
