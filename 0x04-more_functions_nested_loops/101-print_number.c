f537768c408bdaa350ee01b3b2e89b4f3a6df6fa
abetemitope <bestm4305@gmail.com>
2023-08-18 13:18:09 -0700

    corrections

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,42 +8,15 @@
  */
 void print_number(int n)
 {
-	long hn;
-	long n2 = n;
+	unsigned int num = n;
 
-	if (n2 < 0)
-	{
-		_putchar('-');
-		n2 *= -1;
-	}
-	if (n2 / 100000 != 0)
-	{
-		_putchar(hn / 10000 % 10 + '0');
-		_putchar(hn / 1000 % 10 + '0');
-		_putchar(hn / 100 % 10 + '0');
-		_putchar(hn / 10 % 10 + '0');
-		_putchar(hn % 10 + '0');
-	}
-	hn = n2 % 100000;
-	if (hn / 10000 % 10 != 0)
-	{
-		_putchar(hn / 10000 % 10 + '0');
-		_putchar(hn / 1000 % 10 + '0');
-		_putchar(hn / 100 % 10 + '0');
-		_putchar(hn / 10 % 10 + '0');
-	}
-	else if (hn / 1000 % 10 != 0)
-	{
-		_putchar(hn / 1000 % 10 + '0');
-		_putchar(hn / 100 % 10 + '0');
-		_putchar(hn / 10 % 10 + '0');
-	}
-	else if (hn / 100 % 10 != 0)
-	{
-		_putchar(hn / 100 % 10 + '0');
-		_putchar(hn / 10 % 10 + '0');
-	}
-	else if (hn / 10 % 10 != 0)
-		_putchar(hn / 10 % 10 + '0');
-	_putchar(hn % 10 + '0');
+		if (n < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+		if ((num / 10) > 0)
+			print_number(num / 10);
+		_putchar((num % 10) + '0');
+
 }
