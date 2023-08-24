95af83b1bd168e50e38f2f0f681deb7d52bbe2f0
abetemitope <bestm4305@gmail.com>
2023-08-24 04:41:46 -0700

    adjustments in header file.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,13 +4,12 @@
  * File: main.h
  * Auth: Abe Temitope m.
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the Pointers_arrays_strings directory.
  */
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
