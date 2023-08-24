18fb8031ceaf54ce8e5c4fe2a9759d4a5d967a11
abetemitope <bestm4305@gmail.com>
2023-08-24 05:00:27 -0700

    commit

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	char *str;
+	int len;
+	
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
 	return (0);
 }
