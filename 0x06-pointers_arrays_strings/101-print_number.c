04d8b5156c6350ab25a6fcda9bf99c76bbae08a0
abetemitope <bestm4305@gmail.com>
2023-08-24 11:36:48 -0700

    corrections on 101-print_numbers.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,25 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_number - Print the given int one digit at a time
- * @n: The integer to print
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
-	if (n == 0)
+	unsigned int n1;
+
+	if (n < 0)
 	{
-		_putchar('0');
-		return;
-	}
-	else if (n > 0)
-		n *= -1;
-	else
+		n1 = -n;
 		_putchar('-');
-	if ((n / 10) != 0)
-	{
-		print_number((n / 10) * -1);
 	}
-	_putchar((n % 10) * -1 + '0');
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
