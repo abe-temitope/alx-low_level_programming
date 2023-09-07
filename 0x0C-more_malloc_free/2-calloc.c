03109adbfbe311da950b0c55eaf72732103c2438
abetemitope <bestm4305@gmail.com>
2023-09-07 11:16:13 -0700

    Done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
-
 /**
- * _calloc - Allocate memory for array of nmemb elements of size bytes
- * @nmemb: Number of elemnts
- * @size: Size in bytes of elements
- *
- * Return: Pointer to new memory, NULL if it fails
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements.
+ * @size: element's size.
+ * Return: returns a pointer to an array of elements.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
-	unsigned int ar_size, i;
+	unsigned int i;
+	char *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar_size = nmemb * size;
-	ar = malloc(ar_size);
-	if (ar == NULL)
+	array = malloc(nmemb * size);
+	if (array == NULL)
 		return (NULL);
-	while (i < ar_size)
-	{
-		ar[i] = 0;
-		i++;
-	}
-	return (ar);
+	for (i = 0; i < nmemb; i++)
+		*(array + (i * size)) = 0;
+	return (array);
 }
-
